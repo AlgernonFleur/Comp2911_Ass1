20e6b0b478aea5c806754197bac078e34d7afa9b
AlgernonFleur <algernon.fleur@gmail.com>
2017-04-10 10:53:20 +1000

    Screw wrappers, I can just put the formatter in the toString

--- src/TimeInterval.java
+++ src/TimeInterval.java
@@ -1,13 +1,17 @@
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * Created by Algernon on 9/04/2017.
+ */
 public class TimeInterval implements Comparable<TimeInterval>{
-	private Time start;
-	private Time end;
+	private LocalDateTime start;
+	private LocalDateTime end;
 	
 	public TimeInterval(String[] s){
-		this.start = new Time(setTime(s[2],s[3],s[4]));
-		this.end = new Time(setTime(s[5],s[6],s[7]));
+		this.start = setTime(s[2],s[3],s[4]);
+		this.end = setTime(s[5],s[6],s[7]);
 	}
 	
 	private LocalDateTime setTime(String hour, String month, String day){
@@ -18,39 +22,37 @@ public class TimeInterval implements Comparable<TimeInterval>{
 				0,0,0);
 	}
 	
-	public Time getStart() {
+	public LocalDateTime getStart() {
 		return start;
 	}
 	
-	public Time getEnd() {
+	public LocalDateTime getEnd() {
 		return end;
 	}
 	
 	public boolean isIntervalOverlapping(TimeInterval tI){
-		if(		tI.start.getTime().isAfter(this.start.getTime()) &&
-				tI.start.getTime().isBefore(this.end.getTime().plusHours(1)))
-			return true;
-		else if(tI.end.getTime().isAfter(this.start.getTime()) &&
-				tI.end.getTime().isBefore(this.end.getTime().plusHours(1)))
-			return true;
-		else if(tI.start.getTime().isBefore(this.start.getTime()) &&
-				tI.end.getTime().isAfter(this.end.getTime().plusHours(1)))
-			return true;
+		if(		tI.start.isAfter(this.start) &&
+				tI.start.isBefore(this.end.plusHours(1))) return true;
+		else if(tI.end.isAfter(this.start) &&
+				tI.end.isBefore(this.end.plusHours(1))) return true;
+		else if(tI.start.isBefore(this.start) &&
+				tI.end.isAfter(this.end.plusHours(1))) return true;
 		else return false;
 	}
 	
 	@Override
 	public String toString() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH MMM dd");
 		return "TimeInterval{" +
-				"start=" + start +
-				", end=" + end +
+				"start=" + start.format(formatter) +
+				", end=" + end.format(formatter) +
 				'}';
 	}
 	
 	@Override
 	public int compareTo(TimeInterval o) {
-		return Integer.compare(	(int)this.start.getTime().toEpochSecond(ZoneOffset.UTC),
-								(int)o.start.getTime().toEpochSecond(ZoneOffset.UTC));
+		return Integer.compare(	(int)this.start.toEpochSecond(ZoneOffset.UTC),
+								(int)o.start.toEpochSecond(ZoneOffset.UTC));
 	}
 	
 	public static void main(String[] args) {
