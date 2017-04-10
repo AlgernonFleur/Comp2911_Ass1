47eecd9efe3450436e2638758b4ad042c71c5eae
AlgernonFleur <algernon.fleur@gmail.com>
2017-04-10 10:40:38 +1000

    Trying out a wrapper class for time

---
+++ src/Time.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time {
+	private final LocalDateTime localDateTime;
+	
+	public Time(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+	
+	public LocalDateTime getTime() {
+		return localDateTime;
+	}
+	
+	@Override
+	public String toString() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH MMM DD");
+		return this.localDateTime.format(formatter);
+	}
+}
