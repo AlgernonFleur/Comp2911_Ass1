88e9b55d65aec97c8a985f876682b09461695e45
AlgernonFleur <algernon.fleur@gmail.com>
2017-04-10 11:33:09 +1000

    First bit of the Van class

---
+++ src/Van.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Van {
+	private String name;
+	private String depotName;
+	private Transmission transmission;
+	private ArrayList<TimeInterval> bookedTimes;
+}
